b56b539311ef5e9809b9ece98158d6ea0f928b64
Jim <jim@greytag.se>
2023-04-11 17:00:15 +0200

    Web browser settings

--- camera.py
+++ camera.py
@@ -12,27 +12,25 @@ while (True):
         libcamera_process = subprocess.Popen(
             "libcamera-hello -t 0 -f --shutter 400 --framerate 2 -f --brightness 0.3", shell=True)
 
+        os.system("ps -C chromium-browser -o pid=|xargs kill -9")
+
         time.sleep(10)
 
         os.system("ps -C libcamera-hello -o pid=|xargs kill -9")
 
-        time.sleep(5)
+        time.sleep(3)
 
         image_process = subprocess.Popen(
             "libcamera-still -o test.jpg  --shutter 400 --brightness 0.3  -n --immediate", shell=True)
 
-        time.sleep(5)
+        time.sleep(3)
 
-        open_chrome = "/usr/bin/chromium-browser --start-fullscreen ~test.jpg"
-        os.system(open_chrome)
-
-        # webbrowser.open('test.jpg', new=0, autoraise=True)
+        webbrowser.open('test.jpg', new=0, autoraise=True)
 
         time.sleep(10)
 
-        os.system("ps -C chromium-browser -o pid=|xargs kill -9")
-
     except:
 
+        print("Error")
         time.sleep(5)
         continue
